5ac3461cf7047fed4e3021d1ef3cb4077505e8ab
Jonatas Emidio <jonatasemidio@gmail.com>
2023-01-31 22:07:51 +0000

    ad a cat clone that runs 2x faster than GNU cat for large files on Linux

---
+++ cat.js
@@ -0,0 +1,14 @@
+// cat.js
+import { resolve } from "path";
+import { write, stdout, file, argv } from "bun";
+
+const path = resolve(argv.at(-1));
+
+await write(
+  // stdout is a Blob
+  stdout,
+  // file(path) returns a Blob - https://developer.mozilla.org/en-US/docs/Web/API/Blob
+  file(path),
+);
+
+// bun ./cat.js ./README.md
